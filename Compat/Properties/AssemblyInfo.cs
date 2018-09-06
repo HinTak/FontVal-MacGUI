44ff9d3cf52636b44e753df60a60ec967a127204
Hin-Tak Leung <htl10@users.sourceforge.net>
2018-09-06 12:33:11 +0100

    Correct wrong info

--- Compat/Properties/AssemblyInfo.cs
+++ Compat/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Compat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Compat")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyCopyright("Copyright © Hin-Tak Leung 2015-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
