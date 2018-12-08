b1540ca839973e044670193d7c431fd8ee422317
Hin-Tak Leung <htl10@users.sourceforge.net>
2018-12-08 23:25:36 +0000

    forgotten to bump version up!

--- FontValidator/CmdLineInterface.cs
+++ FontValidator/CmdLineInterface.cs
@@ -27,7 +27,7 @@ namespace FontValidator
         List<string>          m_captions = new List<string>();
         bool m_verbose;
         bool m_report2stdout;
-        static string version = "2.1.2";
+        static string version = "2.1.4";
 
         static void ErrOut( string s ) 
         {
